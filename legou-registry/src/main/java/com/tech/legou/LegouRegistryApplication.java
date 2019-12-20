1f7a2dcbd8302b9b62392b20f93e37c593a4b9e5
jingxingqiang123 <1045612194@qq.com>
2019-12-20 23:39:58 +0800

    erruka server first commit

---
+++ legou-registry/src/main/java/com/tech/legou/LegouRegistryApplication.java
@@ -0,0 +1,16 @@
+package com.tech.legou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by jingxingqiang on 2019/12/20 23:15
+ */
+@SpringBootApplication
+@EnableEurekaServer  // 启动eureka服务端
+public class LegouRegistryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LegouRegistryApplication.class,args);
+    }
+}
